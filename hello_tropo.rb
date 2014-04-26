5212886c9ab9afddfcd00b7eee56340aec1e68ed
Shaun Daley <shaundaley39@gmail.com>
2014-04-26 16:40:25 +0100

    hello tropo

---
+++ hello_tropo.rb
@@ -0,0 +1,21 @@
+require 'tropo-webapi-ruby'
+require 'sinatra'
+
+post '/index.json' do
+  
+  t = Tropo::Generator.new
+  
+  t.call(:to => "+447472786867")
+  t.say(:value => "Tag, you're it!")
+
+  t.ask :name => 'color', 
+        :timeout => 60, 
+        :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+        :choices => {:value => "red, blue, green"}
+  
+  t.on :event => 'continue', :next => '/continue.json'
+
+  t.response
+end
+
+
