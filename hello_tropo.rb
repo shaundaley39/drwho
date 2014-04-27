83ed86a807880a002ea2254ac20264e98f4bb24c
Shaun Daley <shaundaley39@gmail.com>
2014-04-27 13:19:32 +0000

    chart updating firehose

--- hello_tropo.rb
+++ hello_tropo.rb
@@ -1,6 +1,7 @@
 require 'tropo-webapi-ruby'
 require 'rubygems'
 require 'sinatra'
+require 'mechanize'
 
 get '/' do
   "hello world! it's #{Time.now} here!"
@@ -10,22 +11,119 @@ end
 #    data = JSON.parse(request.body.read)
 #    json data
 #end
-
 post '/index.json' do
-  
-  t = Tropo::Generator.new
-  
-  t.call(:to => "+447472786867")
-  t.say(:value => "Tag, you're it!")
 
-  t.ask :name => 'color', 
-        :timeout => 60, 
+  t = Tropo::Generator.new
+
+  t.say "Good afternoon Mrs Green! This is a call from Crooksten Medical center to discuss your recent appointment with Dr Who."
+
+  t.ask :name => 'color',
+        :timeout => 60,
+        :say => {:value => "Imagine I just summarized your glucoma consultation. Did you understand all of that?"},
+        :choices => {:value => "no, yes"}
+  t.on :event => 'continue', :next => '/q_further.json'
+
+  t.response
+end
+
+post '/q_further.json' do
+
+  v = Tropo::Generator.parse request.env["rack.input"].read
+
+  t = Tropo::Generator.new
+
+  answer = v[:result][:actions][:color][:value]
+
+  t.say(:value => "You said " + answer)
+  url = "https://sizzling-fire-6880.firebaseio.com/pie/.json"
+  data = { positive:80, negative:10, callback:10}
+
+  Mechanize.new.post url, data.to_json
+
+  t.response
+
+end
+
+
+post '/index3455.json' do
+
+  t = Tropo::Generator.new
+
+  t.call(:to => "+13192142124") #+19168229887")
+  #t.say(:value => "Tag, you're it!")
+  t.say "Welcome to Tropo! Knock knock. Who's there?"
+
+  t.ask :name => 'color',
+        :timeout => 60,
         :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
         :choices => {:value => "red, blue, green"}
-  
   t.on :event => 'continue', :next => '/continue.json'
 
   t.response
+end
+
+post '/index55.json' do
+  
+  t = Tropo::Generator.new
+  t.say "Welcome to Tropo! Knock knock. Who's there?"
+
+  t.ask :name => 'color',
+        :timeout => 60,
+        :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+        :choices => {:value => "red, blue, green"}
+#  t.on :event => 'continue', :next => '/continue.json'
+  t.on :event => 'continue', :next => '/call.json'
+
+
+#  t.call(:to => "+447730466716")
+#  b = Tropo::Generator.new
+#  b.call(:to => "+447730466716")
+#  b.say(:value => "Knock knock. Who's there?")
+
+
+#  t.call(:to => "+447472786867")
+#  t.say(:value => "Tag, you're it!")
+
+#  t.ask :name => 'color', 
+#        :timeout => 60, 
+#        :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+#        :choices => {:value => "red, blue, green"}
+  
+#  t.on :event => 'continue', :next => '/continue.json'
+
+  t.response
+end
+
+post '/continue.json' do
+  
+  v = Tropo::Generator.parse request.env["rack.input"].read
+  
+  t = Tropo::Generator.new
+  
+  answer = v[:result][:actions][:color][:value]
+  
+  t.say(:value => "You said " + answer)
+  
+  t.response
+  
+end
+
+post '/call.json' do
+
+  t = Tropo::Generator.new
+  t.call(:to => "+19168229887")
+
+  t.say(:value => "Knock knock. Who's there?")
+
+  t.ask :name => 'color',
+        :timeout => 60,
+        :say => {:value => "What's your favorite color?  Choose from red, blue or green."},
+        :choices => {:value => "red, blue, green"}
+  t.on :event => 'continue', :next => '/continue.json'
+
+
+  t.response
+  
 end
 
 
